2247fb950017bc71ef8058e6b40c190370e62bb6
ssiddeshwara <ssiddeshwara@manh.com>
2022-06-01 00:26:26 +0530

    added ResponseEntityExceptionHandler

--- src/main/java/com/lrng/blog/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/lrng/blog/exceptions/GlobalExceptionHandler.java
@@ -1,14 +1,18 @@
 package com.lrng.blog.exceptions;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import com.lrng.blog.payloads.ApiResponse;
 
 @RestControllerAdvice
-public class GlobalExceptionHandler {
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler{
 
 	@ExceptionHandler(value = ResourceNotFoundException.class)
 	public ResponseEntity<ApiResponse> resourceNotFoundExceptionHandler(ResourceNotFoundException exception) {
@@ -17,4 +21,11 @@ public class GlobalExceptionHandler {
 		return new ResponseEntity<ApiResponse>(new ApiResponse(message, null, false), HttpStatus.NOT_FOUND);
 	}
 
+	@Override
+	protected ResponseEntity<Object> handleHttpRequestMethodNotSupported(HttpRequestMethodNotSupportedException ex,
+			HttpHeaders headers, HttpStatus status, WebRequest request) {
+		//return super.handleHttpRequestMethodNotSupported(ex, headers, status, request);
+		return new ResponseEntity<Object>(new ApiResponse(ex.getMessage(), null, false), status);
+	}
+	
 }
